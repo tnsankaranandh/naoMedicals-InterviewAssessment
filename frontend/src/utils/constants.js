866ffa38c4f8931a1efa7fe03e558976d963c4cf
Sankaranandh TN <tnsankaranandh@gmail.com>
2026-02-05 03:15:49 +0530

    vercel

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -7,5 +7,5 @@ export const ROLES = {
 // export const SOCKET_URL = "http://localhost:5000";
 
 // For Vercel
-export const API_BASE = "http://localhost:5000/api";
-export const SOCKET_URL = "http://localhost:5000";
+export const API_BASE = "https://nao-medicals-interview-assessment.vercel.app/api";
+export const SOCKET_URL = "https://nao-medicals-interview-assessment.vercel.app";
